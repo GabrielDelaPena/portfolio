d86833a3de352bb894dffa2fed92449e00b1d061
Gabriel Dela Pena <delapena.gabriel12@gmail.com>
2023-05-14 17:14:13 +0200

    Fixing resize bugs.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,36 +17,14 @@ const About = () => {
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
             <motion.div
               className='sm:text-right text-4xl font-bold'
-              /** initial condition */
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true, amount: 0.5 }}
-              transition={{ duration: 0.5 }}
-              variants={{
-                /** final conditions
-                 * x: -50 : div will be at -50 from the start
-                 * x: 0 : div will be at 0 from the start
-                 */
-                hidden: { opacity: 0, x: -50 },
-                visible: { opacity: 1, x: 0 },
-              }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
             >
               <p>Hi. I’m Gabriel, nice to meet you. Please take a look around.</p>
             </motion.div>
             <motion.div
-              /** initial condition */
-              initial="hidden"
-              whileInView="visible"
-              viewport={{ once: true, amount: 0.5 }}
-              transition={{ duration: 0.5 }}
-              variants={{
-                /** final conditions
-                 * x: -50 : div will be at -50 from the start
-                 * x: 0 : div will be at 0 from the start
-                 */
-                hidden: { opacity: 0, x: 50 },
-                visible: { opacity: 1, x: 0 },
-              }}
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
             >
               <p>
                 I am a passionate frontend developer with experience in creating visually stunning and highly functional websites and applications.
